c1fb61568ae50dcc9961672e977622659c51f852
santhoshkumar <its.santhoshkumar@outlook.com>
2024-07-21 13:17:21 +0530

    fix: changes fo allocation on timeline

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ class Test {
     }
 }
 
+// for heap snapshot
+// const globalTest = new Test('globalTest');
+// const globalString = 'globalString';
 
-const globalTest = new Test('globalTest');
-const globalString = 'globalString';
+// // creating scope inside this only it means local scope
+// {
+//     const localTest = new Test('localTest');
+//     const localString = 'localString';
 
-// creating scope inside this only it means local scope
-{
-    const localTest = new Test('localTest');
-    const localString = 'localString';
-
-}
+// }
 
 /**
  * Objects, and array are garpage collection when things are not in use
  * for primitive data type it will does not garpage collected
  */
+
+// Allocation instrumentations on timeline, record specific time need for memory expansions
+document.addEventListener('click', e => {
+    const a = `${e.pageX}`
+
+    console.log('click', a)
+})
